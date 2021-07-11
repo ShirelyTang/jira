25c52d92c67e6127c87fa52cbe4b0376e81b926f
tangting <1104964405@qq.com>
2021-07-11 21:36:56 +0800

    feat:useDebounce

--- src/sscreens/project-list/index.jsx
+++ src/sscreens/project-list/index.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {SearchPanel} from "./search-panel";
 import {List} from "./list";
-import {cleanObject} from "../../utils";
+import {cleanObject, useDebounce, useMount} from "../../utils";
 import * as qs from 'qs'
 
 const apiUrl = process.env.REACT_APP_API_URL
@@ -13,22 +13,24 @@ export const ProjectListScreen = () =>{
     })
     const [uses, setUses] = useState([])
     const [list, setList] = useState([])
+    const debouncedParam = useDebounce(param, 2000)
+
 
     useEffect(()=>{
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async(response)=>{
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async(response)=>{
             if(response.ok){
                 setList(await response.json())
             }
         })
-    },[param])
+    },[debouncedParam])
 
-    useEffect(()=>{
+    useMount(()=>{
         fetch(`${apiUrl}/users`).then(async(response)=>{
             if(response.ok){
                 setUses(await response.json())
             }
         })
-    },[])
+    })
     return(
         <div>
             <SearchPanel  uses={uses} param={param} setParam={setParam}/>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import {useEffect, useState} from "react";
+
 export const isFalsy = (value) => value === 0 ? false : !value
 
 //在一个函数，改变传入的对象本身是不好的
@@ -11,4 +13,37 @@ export const cleanObject = (object) => {
         }
     })
     return result
+}
+
+export  const useMount = (callback) => {
+    useEffect(() => {
+        callback()
+    },[])
+}
+
+// const debounce = (func,delay) =>{
+//     let timeout;
+//     return (...param) => {
+//         console.log('11111111111')
+//         if(timeout){
+//             console.log('222222222')
+//             clearTimeout(timeout)
+//         }
+//         timeout = setTimeout(()=>{
+//             console.log('33333333333333333')
+//             func(...param)
+//         },delay)
+//     }
+// }
+
+//第一个设置的timeOut被第二个清理， 第二个timeOut被第三个清理，只有最后一个会被留下来
+export const useDebounce = (value, delay) => {
+    const[debouncedValue, setDebounceValue] = useState(value)
+    //每次在value变化的时候都会新设置一个定时器， 在 delay 毫秒后就会set一个value
+    useEffect(()=>{
+        const timeout = setTimeout(()=> setDebounceValue(value), delay)
+        //每次在上一个useEffect处理完以后再运行
+        return ()=> clearTimeout(timeout)
+    },[value, delay])
+    return debouncedValue
 }
