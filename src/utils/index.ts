647ef6997ba88521bafe397ffb54f1a9bcc2c41b
tangting <1104964405@qq.com>
2021-07-11 23:05:53 +0800

    feat:给useDebounce添加泛型

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react";
 
-export const isFalsy = (value: any) => value === 0 ? false : !value
+export const isFalsy = (value: unknown) => value === 0 ? false : !value
 
 //在一个函数，改变传入的对象本身是不好的
 // @ts-ignore
@@ -39,7 +39,7 @@ export  const useMount = (callback:()=> void) => {
 // }
 
 //第一个设置的timeOut被第二个清理， 第二个timeOut被第三个清理，只有最后一个会被留下来
-export const useDebounce = (value: any, delay?: number) => {
+export const useDebounce = <V> (value: V, delay?: number): any => {
     const[debouncedValue, setDebounceValue] = useState(value)
     //每次在value变化的时候都会新设置一个定时器， 在 delay 毫秒后就会set一个value
     useEffect(()=>{
